b01d7d18164bcab680a96b9d8c7fb82f2d9d5dee
joseuilton <joseuilton.siqueira@gmai.com>
2024-02-23 14:27:32 -0300

    chore: add create answers table migration

---
+++ api/migrations/20240223172313_create-answers-table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("answers", (table) => {
+    table.uuid("answerId").primary();
+    table.uuid("questionId").notNullable().references("questionId").inTable("questions");
+    table.uuid("userId").notNullable().references("userId").inTable("users");
+    table.string("answer").notNullable();
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").nullable();
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("answers");
+}
+
