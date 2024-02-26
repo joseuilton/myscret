cc44455fc97cb11e89d05f8f78d356a46b06945c
joseuilton <joseuilton.siqueira@gmai.com>
2024-02-26 13:58:54 -0300

    chore: fixing constraint nullable on field pictureUrl on users table

--- api/migrations/20240223185756_add-new-field-pictureUrl-users-table.ts
+++ api/migrations/20240223185756_add-new-field-pictureUrl-users-table.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable("users", (table) => {
-    table.string("pictureUrl").notNullable();
+    table.string("pictureUrl").nullable();
   });
 }
 
