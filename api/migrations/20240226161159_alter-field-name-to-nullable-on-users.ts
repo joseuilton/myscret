25d90aaa53cdfb40292b2df05183c1b190341e5a
joseuilton <joseuilton.siqueira@gmai.com>
2024-02-26 13:45:00 -0300

    chore: alter name field on users table for nullable

---
+++ api/migrations/20240226161159_alter-field-name-to-nullable-on-users.ts
@@ -0,0 +1,16 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table("users", (table) => {
+    table.string("name").nullable().alter();
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table("users", (table) => {
+    table.string("name").notNullable().alter();
+  })
+}
+
